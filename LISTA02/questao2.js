80c1c958ac386160b9b2383e4126449dd811bf9b
Ari Souza <email.arisantos@gmail.com>
2024-11-09 00:36:37 -0300

    add: lista 2 questao 2

--- LISTA02/questao2.js
+++ LISTA02/questao2.js
@@ -1,1 +1,52 @@
-/*Questão 02: Implemente uma pilha em JavaScript que simule a funcionalidade de "desfazer" (undo) em um editor de texto. A cada nova ação (como escrever uma palavra), ela deve ser armazenada na pilha. Quando o usuário clicar no botão "desfazer", a última ação realizada deve ser removida da pilha e o estado anterior do texto deve ser restaurado.*/
+/*Questão 02: Implemente uma pilha em JavaScript que simule a funcionalidade de "desfazer" (undo) em um editor de texto. A cada nova ação
+(como escrever uma palavra), ela deve ser armazenada na pilha. Quando o usuário clicar no botão "desfazer", a última ação realizada deve
+ser removida da pilha e o estado anterior do texto deve ser restaurado.*/
+
+class Pilha {
+
+    constructor(tam = 10) {
+      this.dados = [];
+      this.topo = 0;
+      this.tam = tam;
+      this.currentText = "";
+    }
+    addPalavra(palavra) {
+      if (!this.isFull()) {
+        this.dados[this.topo++] = this.currentText;
+        this.currentText += palavra;
+        return;
+      }
+      throw new Error("Stackoverflow - Pilha ta cheia");
+    }
+  
+    undo() {
+      if (!this.isEmpty()) {
+        this.currentText = this.dados[--this.topo];
+        return;
+      }
+      throw new Error("Stackunderflow - Pilha vazia");
+    }
+  
+    getTexto() {
+      return this.currentText;
+    }
+  
+
+  
+    size() {
+      return this.topo;
+    }
+  
+    isEmpty() {
+      return this.size() === 0;
+    }
+
+    isFull() {
+      return this.size() === this.tam;
+    }
+  
+  }
+  
+  export default Pilha;
+  
+
